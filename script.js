d16473d4a01d15b8dc27e1d2995d65b764c0c1fa
Mohit kachhaway <130671896+MohitKachhway@users.noreply.github.com>
2023-08-13 08:58:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+let p = document.getElementById("para");
+
+let browserName = navigator.appName;
+let version = navigator.appVersion;
+
+p.innerText = `You are using ${browserName} version ${version}`
